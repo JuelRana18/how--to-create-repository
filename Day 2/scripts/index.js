d67e63193064cee49f9ab2db8e39be23a14087cb
JuelRana18 <developerjuelrana@gmail.com>
2024-12-25 16:24:53 +0800

    eCommerce project

---
+++ Day 2/scripts/index.js
@@ -0,0 +1,14 @@
+// Collupsseble menu start
+const menuIcon = document.getElementById ("menu-icon");
+const menu = document.getElementById ("menu");
+
+menuIcon.addEventListener("click" , () => {
+if(menu.className==="hidden"){
+    menu?.classList.remove("hidden");
+}else{
+    menu.classList.add("hidden");
+}
+})
+// Collupsseble menu ends
+
+
